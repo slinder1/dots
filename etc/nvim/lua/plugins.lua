c726fbfe8ad1f73300f28ba1d2322f7e7e01a796
slinder1 <slinder1@amd.com>
2025-08-10 18:37:40 +0000

    dev

--- etc/nvim/lua/plugins.lua
+++ etc/nvim/lua/plugins.lua
@@ -206,5 +206,22 @@ return {
         update_register_on_cycle = true,
       },
     },
+  },
+  {
+    'ggandor/leap.nvim',
+    lazy = false,
+    config = function ()
+      require('leap').set_default_mappings()
+      require('leap').opts.preview_filter =
+        function (ch0, ch1, ch2)
+          return not (
+            ch1:match('%s') or
+            ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
+          )
+        end
+      require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+      require('leap').opts.safe_labels = {}
+      require('leap').opts.labels = 'hjklhgfdsaqwerpoizxcv/.,mn'
+    end,
   }
 }
